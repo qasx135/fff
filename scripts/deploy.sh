405b9be655264f61d1d254c07e6bda4c3a2bf6f9
Константин Орлов <konsor-79@mail.ru>
2025-12-15 09:59:38 +0300

    wip

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -17,7 +17,7 @@ eval $(minikube docker-env)
 echo "📦 [3/8] Установка внешних зависимостей..."
 helm upgrade --install redis oci://registry-1.docker.io/bitnamicharts/redis --set auth.enabled=false \
     --set master.persistence.size=1Gi \
-    -- set replica.persistence.size=1Gi
+    --set replica.persistence.size=1Gi
 
 kubectl wait --for=condition=ready pod -l app.kubernetes.io/name=redis --timeout=100s
 
@@ -25,32 +25,21 @@ helm upgrade --install mongodb oci://registry-1.docker.io/bitnamicharts/mongodb 
     --set auth.enabled=false \
     --set persistence.size=1Gi \
     --set service.port=27017
-kubectl wait --for=condition=ready pod -l app.kubernetes.io/name=mongodb --timeout=100s
 
 helm upgrade --install catalog-db oci://registry-1.docker.io/bitnamicharts/postgresql -f infra/postgres/catalog-db-values.yaml
-kubectl wait --for=condition=ready pod -l app.kubernetes.io/instance=catalog-db --timeout=100s
 helm upgrade --install watch-db oci://registry-1.docker.io/bitnamicharts/postgresql -f infra/postgres/watch-db-values.yaml
-kubectl wait --for=condition=ready pod -l app.kubernetes.io/instance=watch-db --timeout=100s
 helm upgrade --install catalog-db-exporter ./helm/postgres-exporter -f ./infra/postgres-exporter/catalog-db-values.yaml
-kubectl wait --for=condition=ready pod -l app=catalog-db-exporter --timeout=100s
 helm upgrade --install watch-db-exporter ./helm/postgres-exporter -f ./infra/postgres-exporter/watch-db-values.yaml
-kubectl wait --for=condition=ready pod -l app=watch-db-exporter --timeout=100s
 
 helm upgrade --install kafka ./helm/kafka
-kubectl wait --for=condition=ready pod -l component=kafka --timeout=100s
 
 helm upgrade --install opensearch ./helm/opensearch
-kubectl wait --for=condition=ready pod -l app=opensearch --timeout=100s
 helm upgrade --install graylog ./helm/graylog
-kubectl wait --for=condition=ready pod -l app=graylog --timeout=100s
 
 helm upgrade --install prometheus ./helm/prometheus
-kubectl wait --for=condition=ready pod -l app=prometheus --timeout=100s
 helm upgrade --install grafana ./helm/grafana
-kubectl wait --for=condition=ready pod -l app=grafana --timeout=100s
 
 helm upgrade --install jaeger ./helm/jaeger
-kubectl wait --for=condition=ready pod -l app=jaeger --timeout=100s
 
 echo "⛵ [7/8] Сборка и установка ваших сервисов..."
 docker build -t kostuwan/auth-service:latest ./services/auth-service
